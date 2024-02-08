8f6da73b85819eb438adbd056a005c2bc9daf5fa
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-02-08 07:41:49 +0300

    => saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.scss'
+import {log} from 'node:util'
 
 //======================================================================================================
 
@@ -7,6 +8,47 @@ import './App.scss'
 //======================================================================================================
 
 function App() {
+
+
+    type CarType = {
+        model: string
+        year: number
+        on: boolean
+        turnOn: () => void
+        rename: (model: string) => void
+    }
+
+    let car: CarType = {
+        model: 'Reno',
+        year: 2016,
+        on: false,
+        turnOn() {
+            this.on = true
+        },
+        rename(model) {
+            this.model = model
+        }
+    }
+
+    type GarageType = {
+        addCar: (car: CarType) => void
+        logAllCarsNames: () => void
+    }
+
+    function createGarage(): GarageType {
+        let _cars: Array<CarType> = []
+
+        return {
+            addCar(car) {
+                _cars.push(car)
+            },
+            logAllCarsNames() {
+                console.log('cars in the Garage')
+                _cars.forEach(c => console.log())
+            }
+        }
+    }
+
 
     return (
         <div className={'App'}>
