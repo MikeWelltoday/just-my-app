2a2915e1b9ce2e2d8cfb22fc11f4b07c70afa040
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-02-10 10:25:56 +0300

    => saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,21 @@
-import React, {useEffect, useState} from 'react'
-import './App.css'
-import {Link, NavLink, Outlet, useNavigate} from 'react-router-dom'
-import styles from './components/Site.module.css'
-import {S} from './components/pages/__styles'
-import {useWindowSize} from './hooks_OR_helpers/useWindowSize'
+import React from 'react'
+import './App.scss'
+
+//======================================================================================================
+
+
+//======================================================================================================
 
 function App() {
 
-
-    // BACK BUTTON
-    const navigate = useNavigate()
-
-    function onClickHandler() {
-        navigate(-1)
-    }
-
-    // MENU BURGER
-    const windowSize = useWindowSize()
-
     return (
-        <div>
-            <div>
-                <div className={styles.header}><h1>HEADER</h1></div>
-                <div className={styles.body}>
-                    {!(windowSize < 1000) ?
-                        <div className={styles.nav}>
-                            <S.NavWrapper><NavLink to={'/page/0'}>Page1</NavLink></S.NavWrapper>
-                            <S.NavWrapper><NavLink to={'/page/1'}>Page2</NavLink></S.NavWrapper>
-                            <S.NavWrapper><NavLink to={'/page/2'}>Page3</NavLink></S.NavWrapper>
-                            <div><a href="/page1">aHrefPage1</a></div>
-                        </div>
-                        :
-                        <div>NO</div>}
+        <div className={'App'}>
 
-                    <div className={styles.content}>
-                        {/* ДИНАМИЧНАЯ ЧАСТЬ */}
-                        <Outlet/>
-                    </div>
-                </div>
-
-                {/* кнопка возварата */}
-                <Link className={styles.buttonLink} to={'/page/0'}>ГЛАВНАЯ СТРАНИЦА</Link>
-
-                {/*  BACK  */}
-                <button className={styles.buttonLink} onClick={onClickHandler}>НАЗАД</button>
-
-            </div>
         </div>
     )
 }
 
 export default App
+
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import {BrowserRouter, RouterProvider} from 'react-router-dom'
-import {router} from './routes/router'
+
+//===============================================================================================================================================================
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 )
 root.render(
-    // <BrowserRouter>
-    //   <App />
-    // </BrowserRouter>
-
-    <RouterProvider router={router}/>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
