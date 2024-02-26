7eb4c8c102da92d3d02ac2921000e5a451e04c13
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-02-26 18:37:01 +0300

    => saved

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import './App.scss'
+import {useDispatch, useSelector} from 'react-redux'
+import {RootReducerType} from './state/store'
+import {counterDecrementAC, counterIncrementAC} from './state/counter-reducer'
 
 //========================================================================================
 // 🎲 .T.Y.P.E.S.
+
+export type StateType = {
+    count: number
+}
 
 
 //========================================================================================
@@ -10,14 +17,20 @@ import './App.scss'
 
 function App() {
 
+    const count = useSelector<RootReducerType, StateType>(state => state.counter)
+    const dispatch = useDispatch()
+
     return (
         <div className={'App'}>
-
+            <h1>Counter: {count.count}</h1>
+            <button onClick={() => dispatch(counterIncrementAC())}> [ + ]</button>
+            <button onClick={() => dispatch(counterDecrementAC())}> [ - ]</button>
         </div>
     )
 }
 
 export default App
+
 
 
 
