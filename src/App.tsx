6b94019f4f9e77102b2dc77b5a55c985fd33b5b1
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-05-24 14:31:06 +0300

    => saved

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {Error404} from './components/pages/Error404'
 import {S} from './components/pages/_styles'
 import {Model} from './components/pages/Model'
 import {Prices} from './components/pages/Prices'
+import {Form} from './form/Form'
 
 const foo = () => {
     console.log('foo')
@@ -24,36 +25,37 @@ const PATH = {
 function App() {
     return (
         <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE4}>Prices</NavLink></S.NavWrapper>
+            <Form/>
+            {/*    <div className={styles.header}><h1>HEADER</h1></div>*/}
+            {/*    <div className={styles.body}>*/}
+            {/*        <div className={styles.nav}>*/}
+            {/*            <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>*/}
+            {/*            <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>*/}
+            {/*            <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>*/}
+            {/*            <S.NavWrapper><NavLink to={PATH.PAGE4}>Prices</NavLink></S.NavWrapper>*/}
 
-                </div>
-                <div className={styles.content}>
-                    <Routes>
-                        <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}/>
+            {/*        </div>*/}
+            {/*        <div className={styles.content}>*/}
+            {/*            <Routes>*/}
+            {/*                <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}/>*/}
 
-                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
-                        <Route path={PATH.PAGE2} element={<Puma/>}/>
-                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
-                        <Route path={PATH.PAGE4} element={<Prices/>}/>
+            {/*                <Route path={PATH.PAGE1} element={<Adidas/>}/>*/}
+            {/*                <Route path={PATH.PAGE2} element={<Puma/>}/>*/}
+            {/*                <Route path={PATH.PAGE3} element={<Abibas/>}/>*/}
+            {/*                <Route path={PATH.PAGE4} element={<Prices/>}/>*/}
 
-                        {/*<Route path={'/adidas/:id'} element={<Model/>}/>*/}
-                        <Route path={'/:model/:id'} element={<Model/>}/>
+            {/*                /!*<Route path={'/adidas/:id'} element={<Model/>}/>*!/*/}
+            {/*                <Route path={'/:model/:id'} element={<Model/>}/>*/}
 
-                        <Route path={'/*'} element={<Error404/>}/>
+            {/*                <Route path={'/*'} element={<Error404/>}/>*/}
 
-                        {/*<Route path={'/page/error'} element={<Error404/>}/>*/}
-                        {/*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*/}
-                    </Routes>
+            {/*                /!*<Route path={'/page/error'} element={<Error404/>}/>*!/*/}
+            {/*                /!*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*!/*/}
+            {/*            </Routes>*/}
 
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*    <div className={styles.footer}>abibas 2023</div>*/}
         </div>
     )
 }
