6e71e0131ab03912e89f18497d93d6506ede08e7
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2025-03-05 16:17:44 +0300

    => init - 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import './App.css'
 function App() {
 
     return (
-        <>
-            <h1 className={'text-3xl font-bold underline'}>Hello world!</h1>
-        </>
+        <div className={'p-10 border-solid border-2 rounded-lg border-yellow-200 bg-amber-900'}>
+            <div>
+                <h1 className={'text-3xl text-green-500 font-medium'}>Hello world!</h1>
+            </div>
+
+            <footer className={'mt-10'}>footer</footer>
+        </div>
     )
 }
 
